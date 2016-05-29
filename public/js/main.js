5ce3e5ff58893712b6da417073c18f3ea0f91fd3
danil zakablukovskii <danil.kabluk@gmail.com>
2016-05-29 20:21:32 +0200

    moon switcher

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+window.onload = function () {
+    // Moon style toggler
+    const MOON_THEME_CLASS = 'theme-moon',
+        STATES = ['difference', 'hard-light'];
+    var moonSwitchers = document.getElementsByClassName('js-moon'),
+        currentState = 0,
+        sidebar = document.getElementById('sidebar');
+    Array.from(moonSwitchers).forEach(function (el) {
+        el.onclick = function () {
+            let className = document.body.className;
+
+            if (className.indexOf(MOON_THEME_CLASS) < 0) {
+                document.body.className += ' ' + MOON_THEME_CLASS;
+            } else if (currentState === STATES.length) {
+                document.body.className = className.replace(' ' + MOON_THEME_CLASS, '');
+                sidebar.style.backgroundBlendMode = 'normal';
+                currentState = 0;
+            } else {
+                sidebar.style.backgroundBlendMode = STATES[currentState];
+                currentState++;
+            }
+        };
+    });
+};
